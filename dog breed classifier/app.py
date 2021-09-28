30116b27fba3f7d2327c66435644315997646367
Rafael Mata <rafaelmata357@gmail.com>
2021-09-28 14:40:50 -0600

    Feat: Add a new Resnet 50 modelo and load the saved weights trainned before

--- dog breed classifier/app.py
+++ dog breed classifier/app.py
@@ -24,14 +24,21 @@ from keras.preprocessing import image
 import keras    
 import cv2   
 from utils import *
+from classifier import *
 
+import time
 
+@st.cache()
+def tiempo():
+    return time.ctime()
 
 def main():
 
     ''' Main program for the app '''
 
 
+    
+    
     st.set_page_config(
         page_title="Dog Breed Classifier",
         layout="wide",
@@ -45,6 +52,12 @@ def main():
     st.sidebar.markdown('The complete Github repo can be found in [Github](https://github.com/rafaelmata357/Data-Science-Udacity/tree/master/dog%20breed%20classifier)')
     st.sidebar.markdown('[![An old rock in the desert](./linkedinlogo.png)](www.nacion.com)')
 
+    st.write(tiempo())
+    
+    ResNet50_model = import_resnet50_model() # import ResNet 50 pretrainned model with transfer learning
+    path = 'saved_models/weights.best.Resnet50.hdf5' # Path to the best weights Resnet 50 model trainned
+    new_Resnet50_model = load_new_Resnet50(path)
+
     folderPath = st.text_input('Enter folder path:')
 
     
@@ -57,41 +70,16 @@ def main():
             st.write('Procesing Image.... {}'.format(filename.split('/')[-1]))
             st.subheader('Classifier Results:')
             
-    
-   
-        
-   # img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+            scale = 1.35
+            minNeighbors = 4 
+            face_detected, face_image = face_detector2(filename, scale, minNeighbors)
+            if face_detected:
+                st.image(face_image, caption=f"Processed image", width= 400)
 
-    
+            if dog_detector(filename,ResNet50_model):
+                st.write('Guato detectado...')
+         
+    return None    
 
-
-    #if img_file_buffer is not None:
-
-     #   img = Image.open(img_file_buffer)
-      #  with col0:
-       #     st.write('')
-       # with col1:
-        #    st.write('')
-
-        #with col2:
-         #   st.image(
-        #img, caption=f"Processed image", width= 400) #use_column_width=True)
-        #st.write('Procesing Image.... {}'.format(img_file_buffer.name))
-        #data = img_file_buffer.read()
-        #img1 = image.load_img(data, target_size=(224, 224))
-    
-
-        #with col3:
-         #   st.write('')
-    
-
-    #img1 = Image.open(io.BytesIO(img_file_buffer))
-    #img1 = cv2.imread(np.array(img))
-#img = img.convert('RGB')
-#img = img.resize(target_size, Image.NEAREST)
-#img = image.img_to_array(img)
-        
-
-# Call the main process
-
+# Call the main program
 main()
